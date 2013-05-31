ff161cbdd1fa1dc80758fd982b36f2206cb15269
Mark Starkman <mrstarkman@gmail.com>
2013-05-31 13:34:04 -0400

    get the github username from the command line

--- ghsummary.go
+++ ghsummary.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/mstarkman/ghsummary.go/github"
 )
 
+const defaultUserName = ""
+
 func main() {
-	user, err := github.FindUser("mstarkman")
+	githubUsername := flag.String("user", defaultUserName, "the GitHub username to generate the summary")
+	flag.Parse()
+
+	if *githubUsername == defaultUserName {
+		fmt.Println("")
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	user, err := github.FindUser(*githubUsername)
+
 	if err != nil {
 		fmt.Println(err)
 	} else {
